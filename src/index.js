722f34cb502b28267852a37a33093854eacf83d2
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-12-16 00:16:03 +0530

    Trial 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 8080;
-const initialData = require("./InitialData");
+const studentArray = require("./InitialData");
+const maxId = studentArray.length;
 app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
@@ -13,14 +14,14 @@ app.use(bodyParser.json());
 
 app.get("/api/student", (req, res) => {
   try {
-    res.json(initialData);
+    res.json(studentArray);
   } catch (error) {
     res.status(400).send({ message: error.message });
   }
 });
 
 app.get("/api/student/:id", (req, res) => {
-  const student = initialData.find((element) => {
+  const student = studentArray.find((element) => {
     return element.id == req.params.id;
   });
   //   console.log(student);
@@ -29,6 +30,52 @@ app.get("/api/student/:id", (req, res) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+const isNullOrUndefined = (val) => val === null || val === undefined;
+
+app.post("/api/student", (req, res) => {
+  const { name, currentClass, division } = req.body;
+  if (
+    isNullOrUndefined(name) ||
+    isNullOrUndefined(currentClass) ||
+    isNullOrUndefined(division)
+  ) {
+    res.sendStatus(400);
+  } else {
+    const newId = maxId + 1;
+    maxId = newId;
+    const newStudent = {
+      newId,
+      name,
+      currentClass,
+      division,
+    };
+    studentArray.push(newStudent);
+    res.send({ id: newId });
+  }
+});
+
+app.put("/api/student/:id", (req, res) => {
+  const idToUpdate = req.params.id;
+  const { name, currentClass, division } = req.body;
+  const matchedIndex = studentArray.findIndex((s) => s.id == idToUpdate);
+  if (matchedIndex == -1) {
+    res.sendStatus(400);
+  } else {
+    if (!isNullOrUndefined(name)) {
+      studentArray[idToUpdate].name = name;
+      console.log(studentArray);
+    }
+    if (!isNullOrUndefined(currentClass)) {
+      studentArray[idToUpdate].currentClass = currentClass;
+    }
+    if (!isNullOrUndefined(division)) {
+      studentArray[idToUpdate].division = division;
+    }
+    res.sendStatus(200);
+  }
+  // console.log(nameToUpdate.name);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
